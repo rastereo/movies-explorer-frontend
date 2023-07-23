f10482e0ad10aec904a34547bbe094393cfd3585
rastereo <kapturhouse@yandex.ru>
2023-07-23 22:09:09 +0300

    feat: create routes in app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,40 @@ import Main from '../Main';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route
+        path="/"
+        element={<Main />}
+      />
+      <Route
+        path="/movies"
+        element={
+          <h1>Фильмы</h1>
+        }
+      />
+      <Route
+        path="/saved-movies"
+        element={
+          <h1>Сохранённые фильмы</h1>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <h1>Сохранённые фильмы</h1>
+        }
+      />
+      <Route
+        path="/signin"
+        element={
+          <h1>Страницы авторизации</h1>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <h1>Страницы регистрации</h1>
+        }
+      />
     </Routes>
   );
 }
