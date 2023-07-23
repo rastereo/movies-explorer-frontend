ef65da31425303eb2226b872bffea4ef33b13802
rastereo <kapturhouse@yandex.ru>
2023-07-23 19:44:43 +0300

    feat: add fonts, normalize.css, main companent

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Routes, Route } from 'react-router-dom';
+import Main from '../Main';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <h1>Учебный проект студента факультета Веб-разработки.</h1>
+  );
+}
+
+export default Main;
