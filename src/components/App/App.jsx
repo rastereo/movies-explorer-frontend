62ef623348a1c5d283e9708f9b8fc9c3c41abdf7
rastereo <kapturhouse@yandex.ru>
2023-07-24 19:39:09 +0300

    feat: create header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,62 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 import Main from '../Main';
+import Header from '../Header/Header';
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Main />}
+    <>
+      <Header
+        isLoggedIn={isLoggedIn}
       />
-      <Route
-        path="/movies"
-        element={
-          <h1>Фильмы</h1>
-        }
-      />
-      <Route
-        path="/saved-movies"
-        element={
-          <h1>Сохранённые фильмы</h1>
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          <h1>Сохранённые фильмы</h1>
-        }
-      />
-      <Route
-        path="/signin"
-        element={
-          <h1>Страницы авторизации</h1>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <h1>Страницы регистрации</h1>
-        }
-      />
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={<Main />}
+        />
+        <Route
+          path="/movies"
+          element={
+            <h1>Фильмы</h1>
+          }
+        />
+        <Route
+          path="/saved-movies"
+          element={
+            <h1>Сохранённые фильмы</h1>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <h1>Аккаунт</h1>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <h1>Страницы авторизации</h1>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <h1>Страницы регистрации</h1>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <h1>404</h1>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
